960b6ed256ef37d25354bb5911ac827291e20942
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-08-29 13:02:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,21 @@
-
+variable "accessKey" {
+  description = "AWS access key"	  
+}
+variable "secretKey" {
+  description = "AWS secert access key"
+}
+variable "region" {
+default = "ap-south-1"
+}
+variable "vpc_id"{
+default = "vpc-8a0428e2"
+}
+variable "ami_id" {
+default = "ami-0a74bfeb190bd404f"
+}
+variable "instance_type" {
+default = "t2.micro"
+}
+variable "ssh_key_name" {
+default = "new-69aws"
+}
