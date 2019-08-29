4da0de0334f46e27ac521528f4f4ed8b23f131c2
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-08-29 11:58:29 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "access_key" {
+default = "AKIA4LG4UFMXM5H3JLU2"
+}
+variable "secret_key" {
+default = "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"
+}
+variable "region" {
+default = "ap-south-1"
+}
+variable "vpc_id"{
+default = "vpc-8a0428e2"
+}
+variable "ami_id" {
+default = "ami-0a74bfeb190bd404f"
+}
+variable "instance_type" {
+default = "t2.micro"
+}
+variable "ssh_key_name" {
+default = "new-69aws"
+}
